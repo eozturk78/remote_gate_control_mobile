69d202690f7b849d34650ee8efa3c778239c94e5
eozturk78 <eozturk78@gmail.com>
2025-06-23 19:13:40 +0300

    IOS problem is fixed

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,9 +3,8 @@ import Flutter
 import Firebase
 import UserNotifications
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -14,25 +13,16 @@ import UserNotifications
 
     UNUserNotificationCenter.current().delegate = self
 
-    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
-      print("Notification permission granted: \(granted)")
-    }
-
-    application.registerForRemoteNotifications()
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  override func application(_ application: UIApplication,
-                            didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
-  }
-
-  // ÖNEMLİ: override eklenmiş hali
-  override func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                       willPresent notification: UNNotification,
-                                       withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+  // iOS foreground'da iken notification gösterebilmek için:
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
     completionHandler([.alert, .badge, .sound])
   }
 }
